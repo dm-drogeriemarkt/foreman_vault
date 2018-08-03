911ae94bfa28e008a11d3f0376792b15fb8fe865
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2018-08-03 15:38:52 +0200

    Add fetcher to get secrets from vault

--- test/unit/services/foreman_vault/vault_client_test.rb
+++ test/unit/services/foreman_vault/vault_client_test.rb
@@ -4,22 +4,39 @@ require 'test_helper'
 
 class VaultClientTest < ActiveSupport::TestCase
   setup do
-    @url = 'http://127.0.0.1:8200'
-    @token = 'e57e5ef2-b25c-a0e5-65a6-863ab095dff6'
-    @subject = ForemanVault::VaultClient.new(@url, @token)
+    @subject = ForemanVault::VaultClient.new('http://127.0.0.1:8200', 'e57e5ef2-b25c-a0e5-65a6-863ab095dff6')
+    @client = mock
+    Vault::Client.expects(:new).returns(@client)
   end
 
-  describe '#expire_time' do
+  describe '#fetch_expire_time' do
     setup do
       @time = '2018-08-01T20:08:55.525830559+02:00'
       response = OpenStruct.new(data: { expire_time: @time })
-      auth_token = mock('auth_token', lookup: response)
-      client = mock('client', auth_token: auth_token)
-      Vault::Client.expects(:new).returns(client)
+      auth_token = mock
+      auth_token.expects(:lookup).once.with(@subject.instance_variable_get('@token')).returns(response)
+
+      @client.expects(:auth_token).once.returns(auth_token)
     end
 
     test 'should return expire time' do
-      assert_equal Time.zone.parse(@time), @subject.expire_time
+      assert_equal Time.zone.parse(@time), @subject.fetch_expire_time
+    end
+  end
+
+  describe '#fetch_secret' do
+    setup do
+      @secret_path = '/kv/my-secret'
+      @data = { foo: 'bar' }
+      response = OpenStruct.new(data: @data)
+      logical = mock
+      logical.expects(:read).once.with(@secret_path).returns(response)
+
+      @client.expects(:logical).once.returns(logical)
+    end
+
+    test 'should return expire time' do
+      assert_equal @data, @subject.fetch_secret(@secret_path)
     end
   end
 end
